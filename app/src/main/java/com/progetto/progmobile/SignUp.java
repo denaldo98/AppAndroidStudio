ddae41ffd37886f32c3af6439f0547e677fdf97d
denaldo98 <denaldo98@gmail.com>
2020-04-17 16:33:02 +0200

    aggiunta id in signup xml

--- app/src/main/java/com/progetto/progmobile/SignUp.java
+++ app/src/main/java/com/progetto/progmobile/SignUp.java
@@ -1,16 +1,29 @@
 package com.progetto.progmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SignUp extends AppCompatActivity {
+    TextInputLayout regName, regUsername, regEmail, regPhoneNo, regPassword;
+    Button regBtn, regToLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //per togliere la status bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_signup);
+
+        regName = findViewById(R.id.);
+        regUsername = findViewById(R.id.);
+        regEmail = findViewById(R.id.);
+        regPhoneNo = findViewById(R.id.);
+        regPassword = findViewById(R.id.);
+        regBtn = findViewById(R.id.);
+        regToLoginBtn = findViewById(R.id.);
     }
 }
