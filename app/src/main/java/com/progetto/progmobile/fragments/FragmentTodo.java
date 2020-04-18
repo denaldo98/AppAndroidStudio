65067d4be592a389d2daee43e9fe67fcebe85ed3
S1082351 <S1082351@studenti.univpm.it>
2020-04-18 20:41:46 +0200

    continuazione to do

--- app/src/main/java/com/progetto/progmobile/fragments/FragmentTodo.java
+++ app/src/main/java/com/progetto/progmobile/fragments/FragmentTodo.java
@@ -6,14 +6,25 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.progetto.progmobile.R;
 
 public class FragmentTodo extends Fragment {
 
+    RecyclerView reciclerView;
+    String Attivit[];
+    String dateAttivit[];
+    String descrizAttivit[];
+    int[] priorita;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Attivit = getResources().getStringArray(R.array.arrayAttivit);
+        dateAttivit = getResources().getStringArray(R.array.scadenze);
+        descrizAttivit = getResources().getStringArray(R.array.descrizioni);
+        priorita = getResources().getIntArray(R.array.priorita);
+
     }
 
     @Override
