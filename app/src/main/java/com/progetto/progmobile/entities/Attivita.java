22b364b5e4e164d8493a69a5e2ee307f7fb23229
S1082351 <S1082351@studenti.univpm.it>
2020-04-19 15:13:56 +0200

    inizio entities

---
+++ app/src/main/java/com/progetto/progmobile/entities/Attivita.java
@@ -0,0 +1,15 @@
+package com.progetto.progmobile.entities;
+
+public class Attivita {
+    String nome;
+    String data;
+    String descriz;
+    int priorita;
+
+    Attivita (String nome, String data, String descriz, int priorita){
+        this.nome = nome;
+        this.data = data;
+        this.descriz = descriz;
+        this.priorita = priorita;
+    }
+}
