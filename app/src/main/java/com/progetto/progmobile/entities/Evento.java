1a0b58d649fb657264b6c5f36281ba83b49dbd86
denaldo98 <denaldo98@gmail.com>
2020-04-29 20:34:53 +0200

    aggiunta aula all'orario

--- app/src/main/java/com/progetto/progmobile/entities/Evento.java
+++ app/src/main/java/com/progetto/progmobile/entities/Evento.java
@@ -2,12 +2,14 @@ package com.progetto.progmobile.entities;
 
 public class Evento {
     private String nome;
+    private String aula;
     private String giorno;
     private String oraInizio;
     private String oraFine;
 
-    public Evento(String nome, String giorno, String oraInizio, String oraFine) {
+    public Evento(String nome, String aula, String giorno, String oraInizio, String oraFine) {
         this.nome = nome;
+        this.aula = aula;
         this.giorno = giorno;
         this.oraInizio = oraInizio;
         this.oraFine = oraFine;
@@ -16,6 +18,7 @@ public class Evento {
     public String getNome() {
         return nome;
     }
+    public String getAula() { return aula; }
     public String getGiorno() { return giorno; }
     public String getOraInizio() { return oraInizio; }
     public String getOraFine() { return oraFine; }
