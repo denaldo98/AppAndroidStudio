fdbdb8880c627394b58f056f6731551f8ad3ab4d
denaldo98 <denaldo98@gmail.com>
2020-05-02 16:34:02 +0200

    oK

--- app/src/main/java/com/progetto/progmobile/fragments/FragmentCorsi.java
+++ app/src/main/java/com/progetto/progmobile/fragments/FragmentCorsi.java
@@ -10,7 +10,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.progetto.progmobile.AdapterToDo;
+//import com.progetto.progmobile.AdapterToDo;
 import com.progetto.progmobile.R;
 import com.progetto.progmobile.entities.Attivita;
 
