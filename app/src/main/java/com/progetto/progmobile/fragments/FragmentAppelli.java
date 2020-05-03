2b239e858ab991299e9ccc71c9d5c6c0da40a487
S1082351 <S1082351@studenti.univpm.it>
2020-05-03 15:53:40 +0200

    quasi finiti appelli

--- app/src/main/java/com/progetto/progmobile/fragments/FragmentAppelli.java
+++ app/src/main/java/com/progetto/progmobile/fragments/FragmentAppelli.java
@@ -1,12 +1,17 @@
 package com.progetto.progmobile.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,10 +19,12 @@ import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.progetto.progmobile.AdapterAppelli;
 import com.progetto.progmobile.R;
+import com.progetto.progmobile.dialogs.DialogToDoAdd2;
 import com.progetto.progmobile.entities.Appello;
 
 public class FragmentAppelli extends Fragment {
@@ -44,6 +51,68 @@ public class FragmentAppelli extends Fragment {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {  //il primo parametro è per il DRAG che non consideriamo, il secondo paramtro è per le direzione di swipe
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) { //onMove method is for drag and drop
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull final RecyclerView.ViewHolder viewHolder, int direction) { //onSwipe is for swipe movements
+                //AlertDialog per confermare l'eliminazione con lo swipe
+                AlertDialog.Builder removeAlert = new AlertDialog.Builder(getContext());
+                removeAlert.setTitle("Conferma eliminazione");
+                removeAlert.setMessage("Per favore, conferma di voler eliminare l'appello!");
+                removeAlert.setIcon(R.drawable.ic_error_black_24dp);
+
+                removeAlert.setCancelable(false);
+                removeAlert.setPositiveButton("Conferma", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        adapter.deleteItem(viewHolder.getAdapterPosition());
+                        Toast.makeText(getContext(), "Appello eliminato!", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                removeAlert.setNegativeButton("Annulla", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        adapter.notifyDataSetChanged();
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog alert = removeAlert.create();
+                alert.show();
+            }
+        }).attachToRecyclerView(recyclerView);
+
+        adapter.setOnItemClickListener(new AdapterAppelli.OnItemClickListener() {
+            @Override
+            public void onItemClick(DocumentSnapshot documentSnapshot, int position) {
+                Appello appello = documentSnapshot.toObject(Appello.class);
+                String id = documentSnapshot.getId();
+                String path = documentSnapshot.getReference().getPath(); //ottengo il path del documento che posso passare ad un altra activity ad esempio per modificare
+                appello.getMateria();
+                //documentSnapshot.getReference();
+
+                Toast.makeText(getContext(), "Position: " + position + " ID: " + id , Toast.LENGTH_SHORT).show();
+
+                //startActivity(); posso lanciare un altra activity e fare modifiche sul db, devo passare l'id del document!!!!!!!!!!
+            }
+        });
+
+
+        btnAdd = view.findViewById(R.id.button_add_appelli);
+
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogToDoAdd2 dialog = DialogToDoAdd2.newInstance();
+                assert getFragmentManager() != null;
+                dialog.show(getFragmentManager(), "tag");
+            }
+        });
 
         return view;
 
