86562df67828a288867ec0cd50c7a7e975f3d7ed
S1082351 <S1082351@studenti.univpm.it>
2020-05-03 15:30:54 +0200

    appelli lista

--- app/src/main/java/com/progetto/progmobile/fragments/FragmentAppelli.java
+++ app/src/main/java/com/progetto/progmobile/fragments/FragmentAppelli.java
@@ -7,18 +7,23 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.progetto.progmobile.AdapterAppelli;
 import com.progetto.progmobile.R;
+import com.progetto.progmobile.entities.Appello;
 
 public class FragmentAppelli extends Fragment {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-    private CollectionReference todoRef = db.collection("utenti").document(user.getUid()).collection("ToDo");
+    private CollectionReference appelliRef = db.collection("utenti").document(user.getUid()).collection("Appelli");
 
     private AdapterAppelli adapter;
 
@@ -32,8 +37,26 @@ public class FragmentAppelli extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_appelli, container, false);
+        Query query = appelliRef.orderBy("materia", Query.Direction.ASCENDING);
+        FirestoreRecyclerOptions<Appello> options = new FirestoreRecyclerOptions.Builder<Appello>().setQuery(query, Appello.class).build();
+        adapter = new AdapterAppelli(options);
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerviewAppelli);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
 
         return view;
 
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
