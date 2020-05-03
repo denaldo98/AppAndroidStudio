d28a91484d5cef179f9bc540c5ffe21ef282f357
S1082351 <S1082351@studenti.univpm.it>
2020-05-03 15:10:29 +0200

    appelli layout

--- app/src/main/java/com/progetto/progmobile/fragments/FragmentAppelli.java
+++ app/src/main/java/com/progetto/progmobile/fragments/FragmentAppelli.java
@@ -4,12 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.progetto.progmobile.AdapterAppelli;
 import com.progetto.progmobile.R;
 
 public class FragmentAppelli extends Fragment {
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private CollectionReference todoRef = db.collection("utenti").document(user.getUid()).collection("ToDo");
+
+    private AdapterAppelli adapter;
+
+    private ImageButton btnAdd;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
