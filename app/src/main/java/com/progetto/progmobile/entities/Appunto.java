021cd9d79af0d48b9b33a730c4f9071ec9d0cb03
S1082351 <S1082351@studenti.univpm.it>
2020-05-09 12:28:41 +0200

    Appunto class

---
+++ app/src/main/java/com/progetto/progmobile/entities/Appunto.java
@@ -0,0 +1,22 @@
+package com.progetto.progmobile.entities;
+
+public class Appunto {
+    String titolo;
+    String testo;
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public String getTesto() {
+        return testo;
+    }
+
+    public void setTesto(String testo) {
+        this.testo = testo;
+    }
+}
